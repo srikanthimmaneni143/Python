15f963d7c38ad54c97c8eea248ac487bf86bac2c
srikanthimmaneni143 <57127705+srikanthimmaneni143@users.noreply.github.com>
2020-04-23 14:33:25 +0530

    Adding the basic function.py

---
+++ function.py
@@ -0,0 +1,12 @@
+def adding(a,b):
+    return a+b
+
+print("Starting \n")
+
+a=int(input("Enter a num A: "))
+b=int(input("Enter a num B: "))
+print("Addition of ",a,'+',b,' Is: ',adding(a,b))
+
+
+
+
